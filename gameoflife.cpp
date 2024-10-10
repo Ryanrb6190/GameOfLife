c066fa151dcf938a242357d658127d1309ef81e4
Ryan home PC <ryank@HELLRAZZAGAMING>
2024-10-10 19:34:03 +0100

    Added: Operator Overriding for setting cell status and outputting to grid. Scatter Cells function to randomly distribute cells. Count Live Neighbours function to count live cells around a cell.
    WIP:
    Update Grid function to change cells.

--- gameoflife.cpp
+++ gameoflife.cpp
@@ -1,34 +1,55 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib>
 
 using namespace std;
 
 class Cell 
 {
-	private:
-		bool isAlive;
 	public:
-		int xPosition;
-		int yPosition;
-		char icon = 'O';
+		bool isAlive;
+	Cell()
+	{
+		isAlive = NULL;
+	}
+	Cell(bool isAlive) : isAlive(isAlive) {}
+	~Cell()
+	{
+		// Delete any allocated memory used by the object
+	}
 
-		Cell()
-		{
-			xPosition = 0;
-			yPosition = 0;
-			isAlive = true;
-			icon = 'O';
-		}
-		Cell(int xPosition, int yPosition)
-		{
-			this->xPosition = xPosition;
-			this->yPosition = yPosition;
-			isAlive = true;
-		}
-		void birth() {}
-		int numberOfNeighbours() {}
-		void death() { icon = ' ';}
+	// Operator = to set the status of a cell.
+	Cell& operator=(const bool status)
+	{
+		this->isAlive = status;
+		return *this;
+	}
+	char getIcon() const
+	{
+		return (isAlive) ? 'O' : ' ';
+	}
+
 };
+
+// Operator << to print the grid of cells.
+ostream& operator << (ostream& os, const vector<vector<Cell>>& grid)
+{
+	for (const auto& row : grid)
+	{
+		for (const auto& cell : row)
+		{
+			os << "." << cell.getIcon();
+		}
+		os << "." << endl;
+	}
+	return os;
+}
+
+// Operator + to count the total of live neighbours.
+int operator +(const Cell& a, const Cell& b)
+{
+	return (a.isAlive ? 1 : 0) + (b.isAlive ? 1 : 0);
+}
 
 vector<vector<Cell>> generateGrid()
 {
@@ -36,10 +57,10 @@ vector<vector<Cell>> generateGrid()
 	int xSpaces;
 	int ySpaces;
 
-	cout << endl << "Enter number of spaces on the X Axis";
+	cout << endl << "Enter number of spaces on the X Axis: ";
 	cin >> xSpaces;
 
-	cout << endl << "Enter number of spaces on the Y Axis";
+	cout << endl << "Enter number of spaces on the Y Axis: ";
 	cin >> ySpaces;
 
 	vector<vector<Cell>> grid(xSpaces, vector<Cell>(ySpaces));
@@ -47,29 +68,92 @@ vector<vector<Cell>> generateGrid()
 
 	return grid;
 }
-void createCells(vector<vector<Cell>> cellGrid) 
+void createCells(vector<vector<Cell>> &grid) 
 {
-	for (int x = 0; x < cellGrid.size(); x++)
-		for (int y = 0; y < cellGrid[x].size(); y++)
-		{
-			cellGrid[x][y] = *(new Cell());
-		}
-}
-void outputGrid(vector<vector<Cell>> cellGrid)
-{
-	// Outputs the grid to the command line.
-	cout << endl;
-
-	for (int x = 0; x < cellGrid.size(); x++)
+	for (int x = 0; x < grid.size(); x++)
 	{
-		for (int y = 0; y < cellGrid[x].size(); y++)
+		for (int y = 0; y < grid[x].size(); y++)
 		{
-			cout << "." << cellGrid[x][y].icon;
+			grid[x][y] = Cell(false);
 		}
-		cout << "." << endl;
+	}
+}
+
+void outputGrid(vector<vector<Cell>> &grid)
+{
+	// Outputs the grid with the overloaded << operator.
+	cout << grid;
+}
+
+
+void scatterCells(vector<vector<Cell>> &grid)
+{
+	int numCells;
+	int totalCells = 0;
+
+	srand(time(0)); // Generate a new seed
+
+	cout << endl << "Enter total of alive cells: ";
+	cin >> numCells;
+	
+	// Ensure the number of live cells is not greater than the total grid spaces.
+	int maxCells = grid.size() * grid[0].size();
+	if (numCells > maxCells)
+	{
+		numCells = maxCells;
 	}
 
+	while (totalCells < numCells)
+	{	
+		int xPos = rand() % grid.size();
+		int yPos = rand() % grid[0].size();
+
+		// If the cell is not already alive
+		if (!grid[xPos][yPos].isAlive)
+		{
+			grid[xPos][yPos] = 1;
+			totalCells++;
+		}
+	}
 }
+
+// Count the total of live cells around cell at grid (x, y)
+int countLiveNeighbours(const vector<vector<Cell>>& grid, int x, int y)
+{
+	int liveNeighbours = 0;
+	int rows = grid.size();
+	int cols = grid[0].size();
+
+	for (int i = -1; i <= 1; ++i)
+	{
+		for (int j = -1; j <= 1; ++j)
+		{
+			if (i == 0 && j == 0) { continue; } // Ignore self.
+			int newX = x + i;
+			int newY = y + j;
+
+			// Ensure the indices are within bounds
+			if ( (newX >= 0 && newX < rows) && (newY >= 0 && newY < cols) )
+			{
+				liveNeighbours += grid[newX][newY].isAlive ? 1 : 0;
+			}
+		}
+	}
+	return liveNeighbours;
+}
+
+void UpdateGrid(vector<vector<Cell>> &grid)
+{
+	for (int x = 0; x < grid.size(); x++)
+	{
+		for (int y = 0; y < grid.size(); y++)
+		{
+			
+		}
+	}
+}
+
+
 void runSimulation() 
 {
 	// Runs the simulation for x cycles.
@@ -89,6 +173,10 @@ int main()
 {
 	vector<vector<Cell>> grid = generateGrid();
 	createCells(grid);
+	scatterCells(grid);
 	outputGrid(grid);
+
+	int liveNeighbours = countLiveNeighbours(grid, 2, 2);
+	cout << endl << "Live neighbours of cell (2, 2): " << liveNeighbours << endl;
 	return 0;
 }
